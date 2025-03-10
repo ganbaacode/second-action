0edde048fd51f45b4b7903fa928aa39ee0919211
ganbayar tsogbadrakh <ganbayartsogbadrakh@ganbayars-MacBook-Pro.local>
2025-03-10 15:14:26 -0400

    yml beautify

--- .circleci/writeCircleCiConfig.js
+++ .circleci/writeCircleCiConfig.js
@@ -12,73 +12,76 @@ const writeCircleCiConfig = async (config) => {
 
     let yaml = `version: 2.1
 orbs:
-    node: circleci/node@4.1.0
+ node: circleci/node@4.1.0
 parameters:
-    GHA_Action:
-        type: string
-        default: ''
-    GHA_CommitSha:
-        type: string
-        default: ''
-    GHA_Workflow_Name:
-        type: string
-        default: ''
-    GHA_DeployTag:
-        type: string
-        default: ''
+  GHA_Action:
+    type: string
+    default: ''
+  GHA_CommitSha:
+    type: string
+    default: ''
+  GHA_Workflow_Name:
+    type: string
+    default: ''
+  GHA_DeployTag:
+    type: string
+    default: ''
 
 executors:
-    ubuntu:
-        machine:
-            image: ubuntu-2004:202111-01
-            docker_layer_caching: false
-    ubuntu-caching:
-        machine:
-            image: ubuntu-2004:202111-01
-            docker_layer_caching: true
+  ubuntu:
+    machine:
+      image: ubuntu-2004:202111-01
+      docker_layer_caching: false
+  ubuntu-caching:
+    machine:
+      image: ubuntu-2004:202111-01
+      docker_layer_caching: true
 
 workflows:
-    version: 2
-    pull_request:
-        when: ["", << pipeline.parameters.GHA_Action >> ]
-        jobs:
-            - build:
-                context: org-global
-                filters:
-                    branches:
-                        only:
-                            - main
-            - test:
-                context: org-global
-                requires:
-                    - build
-                filters:
-                    branches:
-                        only:
-                            - main
-    ${process.env.GHA_wWorkflow_Name || 'deploy_app'}:
-        when: ["deploy_app", << pipeline.parameters.GHA_Action >> ]
-        jobs:
-            - deploy:
-                context: org-global
-                requires:
-                    - test
-                filters:
-                    branches:
-                        only:
-                            - main
-    cut_release:
-        when: ["cut_release", << pipeline.parameters.GHA_Action >> ]
-        jobs:
-            - cut_release:
-                executor: ubuntu
-                resource_class: small
-                steps:
-                    - checkout
-                    - node/install
-                    - run:
-                        name: Cut Release
-                        command: bash .circleci/scripts/cut_release.sh -a "<< pipeline.parameters.GHA_CommitSha >>"
+  version: 2
+
+  pull_request:
+    when: ["", << pipeline.parameters.GHA_Action >> ]
+    jobs:
+      - build:
+          context: org-global
+          filters:
+            branches:
+              only:
+                - main
+      - test:
+          context: org-global
+          requires:
+            - build
+          filters:
+            branches:
+              only:
+                - main
+  ${process.env.GHA_wWorkflow_Name || 'deploy_app'}:
+    when: 
+      equal: ["deploy_app", << pipeline.parameters.GHA_Action >> ]
+    jobs:
+      - deploy:
+          context: org-global
+          requires:
+            - test
+          filters:
+            branches:
+              only:
+                - main
+  cut_release:
+    when: 
+      equal: ["cut_release", << pipeline.parameters.GHA_Action >> ]
+    jobs:
+      - cut_release:
+          executor: ubuntu
+          resource_class: small
+          steps:
+            - checkout
+            - node/install
+          - run:
+              name: Cut Release
+              command: bash .circleci/scripts/cut_release.sh -a "<< pipeline.parameters.GHA_CommitSha >>"
     `;
     await writeFile(path.join(__dirname, '../.circleci/new_config.yml'), yaml);
 }
